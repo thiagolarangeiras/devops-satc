ecd2bbfd6c34f31314f5de360dcfd98a5de3b7b5
thiagolarangeiras <thiago.larangeira50@gmail.com>
2026-04-28 08:55:31 -0300

    ajustes front

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: 8000,
-  },
   plugins: [react()],
   resolve: {
     alias: {
